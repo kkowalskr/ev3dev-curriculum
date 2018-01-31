f97fe5c03c8323b89a10987395a6a6be8386c237
giraldpa <giraldpa@rose-hulman.edu>
2018-01-31 14:29:50 -0500

    m3

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -18,6 +18,14 @@ import time
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
+    def __init__(self):
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
     
+
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
