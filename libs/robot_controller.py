9981b611dddefce37273d121742d791af88c8044
kowalskr <kowalskr@rose-hulman.edu>
2018-01-31 14:55:27 -0500

    updated robot_controller for m4, added turn_degrees

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -43,8 +43,12 @@ class Snatch3r(object):
 
         pos = degrees * 90
 
-        self.left_motor.run_to_rel_pos(position_sp = pos, speed_sp = sp)
-        self.right_motor.run_to_rel_pos(position_sp = pos, speed_sp = -sp)
+        self.left_motor.run_to_rel_pos(position_sp = pos, speed_sp = sp,stop_action=
+                                  ev3.Motor.STOP_ACTION_BRAKE)
+        self.right_motor.run_to_rel_pos(position_sp = pos, speed_sp = -sp, stop_action=
+                                  ev3.Motor.STOP_ACTION_BRAKE)
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
         # TODO: Implement the Snatch3r class as needed when working the sandox exercises
         # (and delete these comments)
