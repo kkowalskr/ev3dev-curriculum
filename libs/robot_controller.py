6390e45133e36d4977595d06df3873cfd9ca0ea0
giraldpa <giraldpa@rose-hulman.edu>
2018-02-01 13:53:40 -0500

    fixing some positioning

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -35,13 +35,13 @@ class Snatch3r(object):
         self.left_motor.run_to_rel_pos(position_sp=pos, speed_sp=sp,
                                        stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.right_motor.run_to_rel_pos(position_sp=pos, speed_sp=sp,
-                                        stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+                                       stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
     def turn_degrees(self, degrees, sp):
         """Turns the robot a given amount of degrees at a speed"""
-        pos = int(degrees*4.6)
+        pos = int(degrees * 4.6)
 
         self.left_motor.run_to_rel_pos(position_sp=pos, speed_sp=sp,
                                        stop_action=
