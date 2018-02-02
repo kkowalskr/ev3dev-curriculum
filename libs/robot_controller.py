af763419b24ebf349b5643bee4dcbbc0732adf27
kowalskr <kowalskr@rose-hulman.edu>
2018-02-02 13:33:54 -0500

    finished m4 code. Needs Testing

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -35,7 +35,7 @@ class Snatch3r(object):
         self.left_motor.run_to_rel_pos(position_sp=pos, speed_sp=sp,
                                        stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.right_motor.run_to_rel_pos(position_sp=pos, speed_sp=sp,
-                                       stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+                                        stop_action=ev3.Motor.STOP_ACTION_BRAKE)
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
