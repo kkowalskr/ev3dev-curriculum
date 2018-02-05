d22fa14a8ff056b174dd3efbcdb52588d1bf17c8
giraldpa <giraldpa@rose-hulman.edu>
2018-02-05 14:40:44 -0500

    changed lego number to 21

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -98,4 +98,4 @@ class Snatch3r(object):
         ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
         ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
         print('Goodbye')
-        ev3.Sound.speak('Goodbye').wait(.01)
+        ev3.Sound.speak('Goodbye').wait()
